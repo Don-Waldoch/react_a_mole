0f043cdec0a866fb34047a15d8a050941f998c20
unknown <waldoch.don@gmail.com>
2022-04-09 15:32:03 -0500

    React-A-Mole MVP

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'; // I limit the image size
+import { useState } from 'react'
+import MoleContainer from './Components/MoleContainer'
 
 function App() {
+  let [score, setScore] = useState(0)
+
+  const createMoleHills = () => {
+    let hills = []
+    for (let i=0; i<9; i++) {
+      hills.push(
+        <
+          MoleContainer
+          key={i}
+          setScore={setScore}
+          score={score}
+        />
+      )
+    }
+    
+    return (
+      <div>{hills}</div>
+    )
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>React-a-Mole!</h1>
+      <h3>Whack a Mole When One Appears!</h3>
+      Score: {score}
+      {createMoleHills()}
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
